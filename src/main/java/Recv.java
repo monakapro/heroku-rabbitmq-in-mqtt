6110555c1afa0658862b03b27edcd275649ad18f
Ono Hiroyuki <hiroyuki.ono@flect.co.jp>
2018-07-18 15:00:47 +0900

    change

---
+++ src/main/java/Recv.java
@@ -0,0 +1,35 @@
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import java.io.IOException;
+
+public class Recv {
+
+  private static final String QUEUE_NAME = "rabbitmq_sample";
+
+  public static void main(String[] argv) throws Exception {
+    ConnectionFactory connectionFactory = new ConnectionFactory();
+    connectionFactory.setUsername("ycudoyez");
+    connectionFactory.setPassword("wP4zaI_pvwfob4UOPc4FGgxkyf-oz_kC");
+    connectionFactory.setHost("rhino.rmq.cloudamqp.com");
+    Connection connection = connectionFactory.newConnection();
+    Channel channel = connection.createChannel();
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+    Consumer consumer =
+        new DefaultConsumer(channel) {
+          @Override
+          public void handleDelivery(
+              String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+              throws IOException {
+            String message = new String(body, "UTF-8");
+            System.out.println("Received '" + message + "'");
+          }
+        };
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+  }
+}
